054c5a8f4a5f8c459725a6609f94bdd7619a7e5a
Srujan U <srujanu@gmail.com>
2022-07-05 21:27:53 +0530

    Create 7.FormattedString.py

---
+++ 7.FormattedString.py
@@ -0,0 +1,12 @@
+first = 'Srujan'
+last = 'U'
+
+#Expected Output - Srujan [U] is a Filmaker
+
+#Causal Method:
+message = first + '[ '+ last + '] is a Filmaker'
+#print(message)
+
+#Using Formatted String
+message1 = f'{first} [ {last} ] is a Filmaker'
+print = (message1)
