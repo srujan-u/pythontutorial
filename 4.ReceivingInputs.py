a3dee2e1bf0df1785fda42abc13671ceeeb64bf2
Srujan U <srujanu@gmail.com>
2022-07-05 20:38:33 +0530

    Update 4.ReceivingInputs.py

--- 4.ReceivingInputs.py
+++ 4.ReceivingInputs.py
@@ -1,1 +1,6 @@
+#You need to assign the values received from the input statement to save it to any variable
 
+name = input('What is your name? ')
+print('Hi ' + name)
+
+#Printing multiple content?, You just have to add a '+' in between the values while printing
