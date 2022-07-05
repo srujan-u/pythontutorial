544516f1323b774f03d7bf4c3dcf883d9e9ed8f8
Srujan U <srujanu@gmail.com>
2022-07-05 20:24:24 +0530

    Create 3.Variables.py

---
+++ 3.Variables.py
@@ -0,0 +1,9 @@
+#Variables are created and used directly by assigning values to them
+
+var = 10
+print(var)
+
+#printing variables doesn't need them to be included in quotes as they are not string
+
+#Output - 10
+
