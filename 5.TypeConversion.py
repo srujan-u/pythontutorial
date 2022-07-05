09225fe11970c0fbb66136e91f255eb93c0f66f1
Srujan U <srujanu@gmail.com>
2022-07-05 20:53:13 +0530

    Create 5.TypeConversion.py

---
+++ 5.TypeConversion.py
@@ -0,0 +1,8 @@
+#Type Conversion
+
+birthyear = input('What is your birth year? ')
+#Calculating Age, need to convert birthyear value from string to int for operation
+
+age = 2022 - int(birthyear)
+#Printing Age
+print(age)
