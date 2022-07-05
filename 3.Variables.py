a9446dce264b87969ae7ece490411de9e356ff05
Srujan U <srujanu@gmail.com>
2022-07-05 20:30:11 +0530

    Update 3.Variables.py

--- 3.Variables.py
+++ 3.Variables.py
@@ -1,6 +1,9 @@
 #Variables are created and used directly by assigning values to them
 
 var = 10
+rating = 4.7
+name = 'Srujan'
+
 print(var)
 
 #printing variables doesn't need them to be included in quotes as they are not string
