39a327a3e1f2647e43fbb7e8858e400bd4bbe902
Srujan U <srujanu@gmail.com>
2023-10-03 11:14:39 +0530

    Create 5.1. User Weight.py

---
+++ 5.1. User Weight.py
@@ -0,0 +1,7 @@
+#Get user weigth in lbs and convert them to kgs.
+weightp = input('Enter your Weight in Pounds: ')
+#convert the input type to int for weightp
+#1lbs = 0.45
+weightkg = int(weightp) * 0.45
+print(weightkg)
+#Output - User weight in Kgs.
