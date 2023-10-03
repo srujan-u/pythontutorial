54daf263df61c44b318b4bff69fe5f30dcc3e856
Srujan U <srujanu@gmail.com>
2023-10-03 11:44:58 +0530

    Create 8. String Lenght.py

---
+++ 8. String Lenght.py
@@ -0,0 +1,5 @@
+task = "I'm learning Python"
+#To find the lenght of the String - Use len()
+tasklen = len(task)
+#len() returns the value of the number of letters in the given string
+print(tasklen)
