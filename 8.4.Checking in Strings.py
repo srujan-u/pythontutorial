6a587987d8d29877cfb47b7425f6c81b7c75f77f
Srujan U <srujanu@gmail.com>
2023-10-03 12:07:45 +0530

    Create 8.4.Checking in Strings.py

---
+++ 8.4.Checking in Strings.py
@@ -0,0 +1,5 @@
+task = "I'm Learning Python"
+#"string' in Variable - 'in' is used in this statement to check if the Strings entered is avaialable in the original String.
+#this is case senstitive and the output is always in Boolean Values.
+print('Python' in task)
+print("python" in task)
