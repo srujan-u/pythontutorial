238b14d83d4ffd88f9cc987ac20cc8225a558a75
Srujan U <srujanu@gmail.com>
2023-10-03 12:54:12 +0530

    Create 10. Math Functions.py

---
+++ 10. Math Functions.py
@@ -0,0 +1,8 @@
+#Rounding up the digits
+x = 2.9
+print(round(x))
+
+#Absolute Operation - Always returns a positive number
+print(abs(-39.2))
+
+#math module in python 3 has list of built in operations. use it with import math
