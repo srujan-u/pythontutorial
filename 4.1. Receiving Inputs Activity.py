7a5729446983eee41d95df5c98c6a54584793856
Srujan U <srujanu@gmail.com>
2023-10-03 10:56:41 +0530

    Create 4.1. Receiving Inputs Activity.py

---
+++ 4.1. Receiving Inputs Activity.py
@@ -0,0 +1,10 @@
+#Activity to enter 2 names and colours. Output as 'Srujan likes Red'
+name1 = input("What is your name? ")
+colour1 = input("What is your favourite colour? ")
+
+name2 = input("What is your name? ")
+colour2 = input("What is your favourite colour? ")
+
+print(name1 + ' likes ' + colour1 )
+print(name2 + ' likes ' + colour2 )
+
