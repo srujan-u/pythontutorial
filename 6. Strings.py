5077e8476bc7c7669404d8dd285d30cc92abd527
Srujan U <srujanu@gmail.com>
2023-10-03 11:32:46 +0530

    Create 6. Strings.py

---
+++ 6. Strings.py
@@ -0,0 +1,19 @@
+#Using String with Single Quotes - helpful when using double quotes inbetween the strings
+
+dialogue = 'he said "why are you here?" '
+print(dialogue)
+
+#Using Strings with Double Quotes - Helpful when using single quotes in the strings
+
+dialogue1 = " Who's this?"
+print(dialogue1)
+
+#writing multi line strings using 3 single quotes
+
+dialogue2 = '''
+Hey there,
+I'm glad you're here
+This conversation might keep on going. See you soon.
+'''
+
+
