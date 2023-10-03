dcfae53fac72a9e655834128fe0b7d598929e1f7
Srujan U <srujanu@gmail.com>
2023-10-03 12:22:36 +0530

    Create 9.Arithemetic Operations.py

---
+++ 9.Arithemetic Operations.py
@@ -0,0 +1,19 @@
+add = 7+4
+print(add)
+sub = 7-4
+print(sub)
+mul = 7*4
+print(mul)
+#this div operator '/' will return the float value
+div = 7/4
+print(div)
+#This div operator '//' will return integer value
+div1 = 7//4
+print(div1)
+#Modulus operator will  return the remainder of the division
+mod = 7%4
+print(mod)
+#Exponential operator - 7 to the power 4
+exp = 7**4
+print(exp)
+
