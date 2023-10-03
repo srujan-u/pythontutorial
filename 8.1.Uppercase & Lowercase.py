5563c76026a329018e94525a66315a0f8146e60c
Srujan U <srujanu@gmail.com>
2023-10-03 11:51:39 +0530

    Create 8.1.Uppercase & Lowercase.py

---
+++ 8.1.Uppercase & Lowercase.py
@@ -0,0 +1,11 @@
+task = "I'm learning Python"
+print(task)
+#converting the given string to UPPERCASE
+uppermessage = task.upper()
+lowermessage = task.lower()
+#Printing in Uppercase
+#print(uppermessage)
+print(task.upper())
+#Printing in Lowercase
+#print(lowermessage)
+print(task.lower())
