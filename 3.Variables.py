d205c6ddd6c6949b7724c8943e407705abbf60f7
Srujan U <srujanu@gmail.com>
2023-10-03 10:42:14 +0530

    Update 3.Variables.py

--- 3.Variables.py
+++ 3.Variables.py
@@ -1,6 +1,7 @@
 # This code is printing an expression.
 # '*' is considered as a string, and it's followed by multiplication operator followed by the number. The output of the code will be the the string in the paranthesis printed with the number you mention after it
 # In this case it's 5.
+#Variables should always contain small letters
 print('*' * 5)
 # Output - *****
 
