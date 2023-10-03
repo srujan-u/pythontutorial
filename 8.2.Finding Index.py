57452ee3744d1fb35e03a32fc5e202aa3a3ca2bc
Srujan U <srujanu@gmail.com>
2023-10-03 11:58:05 +0530

    Update 8.2.Finding Index.py

--- 8.2.Finding Index.py
+++ 8.2.Finding Index.py
@@ -3,3 +3,6 @@ print(task.find('P'))
 #Here the value of the P will be 13 as it will also count the spaces inbetween.
 print(task.find('I'))
 #the value printed for the last print statement will be 0
+print(task.find('0'))
+#if the value of the entered value doesn't exists, it'll return the value -1.
+#The output of the last print statement will be -1.
