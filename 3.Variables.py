14a99bf69b60f6e993cd2d47eea15073a0dd81ee
Srujan U <srujanu@gmail.com>
2023-10-03 10:39:53 +0530

    Create 3.Variables.py

---
+++ 3.Variables.py
@@ -0,0 +1,8 @@
+# This code is printing an expression.
+# '*' is considered as a string, and it's followed by multiplication operator followed by the number. The output of the code will be the the string in the paranthesis printed with the number you mention after it
+# In this case it's 5.
+print('*' * 5)
+# Output - *****
+
+#Print a string with repeating it 10 times
+print('bla' * 10)
