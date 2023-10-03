0c25122a7f9557f0a3b261adb3390950aa969a28
Srujan U <srujanu@gmail.com>
2023-10-03 11:33:13 +0530

    Create 6.1. Strings & Arrangements.py

---
+++ 6.1. Strings & Arrangements.py
@@ -0,0 +1,22 @@
+#Printing letter with positing from a string
+dialogue3 = 'Letters are in the order'
+print(dialogue3[0])
+# 0 refers to the first letter of the string
+#-1 refers to last letter of the string, -2 referes to the previous of the last letter of the string and similarly others
+
+#Printing multiple letters from the string in order
+print(dialogue3[0:7])
+
+#this will print the whole string as there is no mentioned upper limit. It'll print the string from the position mentioned in the lower limit
+print(dialogue3[0:])
+
+#printing from Second letter to Last letter in the string
+print(dialogue3[1:-1])
+#You can also assign these values to variable
+
+
+#Outputs -
+#L
+#Letters
+#Letters are in the order
+#etters are in the orde
