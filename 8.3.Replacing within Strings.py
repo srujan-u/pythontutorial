7852ce76892d8b85e6f0a75ab5505f982e81be8c
Srujan U <srujanu@gmail.com>
2023-10-03 12:02:36 +0530

    Create 8.3.Replacing within Strings.py

---
+++ 8.3.Replacing within Strings.py
@@ -0,0 +1,6 @@
+task = "I'm Learning Python"
+print(task.replace('Python',"Python Better"))
+#Here the initial string will be replaced with the updated part of the string
+print(task.replace('Learning', 'Learn'))
+#this print replace statement will not effect the original given String.
+print(task)
