3f56d27f356280a8c80630e3914104dd5b000ef9
Srujan U <srujanu@gmail.com>
2023-10-03 11:55:43 +0530

    Create 8.2.Finding Index.py

---
+++ 8.2.Finding Index.py
@@ -0,0 +1,5 @@
+task = "I'm Learning Python"
+print(task.find('P'))
+#Here the value of the P will be 13 as it will also count the spaces inbetween.
+print(task.find('I'))
+#the value printed for the last print statement will be 0
